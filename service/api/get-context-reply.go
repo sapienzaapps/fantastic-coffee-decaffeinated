56b0aad59aa0e1ccbdfa5219d5717f4b0a0686f9
Enrico204 <enrico204@gmail.com>
2022-11-07 17:20:44 +0100

    Fix example comment in API package

--- service/api/get-context-reply.go
+++ service/api/get-context-reply.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-// getHelloWorld is an example of HTTP endpoint that returns "Hello World!" as a plain text. The signature of this
+// getContextReply is an example of HTTP endpoint that returns "Hello World!" as a plain text. The signature of this
 // handler accepts a reqcontext.RequestContext (see httpRouterHandler).
 func (rt *_router) getContextReply(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
 	w.Header().Set("content-type", "text/plain")
